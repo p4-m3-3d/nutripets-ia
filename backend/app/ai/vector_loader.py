19ea5f7a5c08bef15e6bbe7ff52fb9476ec16190
p4-m3-3d <correolol369@gmail.com>
2026-06-07 13:19:30 -0400

    implemetanciones nuevas fase 8

---
+++ backend/app/ai/vector_loader.py
@@ -0,0 +1,115 @@
+from app.ai.chroma_client import collection
+from app.ai.embedding_service import create_embedding
+
+from app.core.database import SessionLocal
+
+from app.models.recipe_model import Recipe
+from app.models.advice_model import Advice
+from app.models.faq_model import FAQ
+from app.models.toxic_ingredient_model import ToxicIngredient
+
+
+class VectorLoader:
+
+    @staticmethod
+    def load_all():
+
+        db = SessionLocal()
+
+        try:
+
+            collection.delete(
+                where={}
+            )
+
+        except:
+            pass
+
+        recipes = db.query(Recipe).all()
+
+        for recipe in recipes:
+
+            text = f"""
+            Receta:
+            {recipe.title}
+
+            {recipe.description}
+            """
+
+            collection.add(
+                ids=[f"recipe_{recipe.id}"],
+                documents=[text],
+                embeddings=[
+                    create_embedding(text)
+                ]
+            )
+
+        advice_list = db.query(Advice).all()
+
+        for advice in advice_list:
+
+            text = f"""
+            Consejo:
+            {advice.title}
+
+            {advice.content}
+            """
+
+            collection.add(
+                ids=[f"advice_{advice.id}"],
+                documents=[text],
+                embeddings=[
+                    create_embedding(text)
+                ]
+            )
+
+        faq_list = db.query(FAQ).all()
+
+        for faq in faq_list:
+
+            text = f"""
+            Pregunta:
+            {faq.question}
+
+            Respuesta:
+            {faq.answer}
+            """
+
+            collection.add(
+                ids=[f"faq_{faq.id}"],
+                documents=[text],
+                embeddings=[
+                    create_embedding(text)
+                ]
+            )
+
+        toxic_list = db.query(
+            ToxicIngredient
+        ).all()
+
+        for toxic in toxic_list:
+
+            text = f"""
+            Ingrediente:
+            {toxic.name}
+
+            Síntomas:
+            {toxic.symptoms}
+
+            Acción:
+            {toxic.action_required}
+            """
+
+            collection.add(
+                ids=[f"toxic_{toxic.id}"],
+                documents=[text],
+                embeddings=[
+                    create_embedding(text)
+                ]
+            )
+
+        db.close()
+
+        return {
+            "message": "Base vectorial creada"
+        }
